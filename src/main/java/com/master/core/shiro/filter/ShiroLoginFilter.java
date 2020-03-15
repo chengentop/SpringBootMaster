76a433b268cd4b7f962a68c011408e18b8bada35
chengen <18291710694@163.com>
2020-03-15 12:45:47 +0800

    添加shir未登录拦截 返回json

---
+++ src/main/java/com/master/core/shiro/filter/ShiroLoginFilter.java
@@ -0,0 +1,55 @@
+package com.master.core.shiro.filter;
+
+import com.alibaba.fastjson.JSONObject;
+import org.apache.shiro.web.filter.authc.FormAuthenticationFilter;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ShiroLoginFilter extends FormAuthenticationFilter  {
+    /**
+     * 如果isAccessAllowed返回false 则执行onAccessDenied
+     * @param request
+     * @param response
+     * @param mappedValue
+     * @return
+     */
+    @Override
+    protected boolean isAccessAllowed(ServletRequest request, ServletResponse response, Object mappedValue) {
+        if (request instanceof HttpServletRequest) {
+            if (((HttpServletRequest) request).getMethod().toUpperCase().equals("OPTIONS")) {
+                return true;
+            }
+        }
+        return super.isAccessAllowed(request, response, mappedValue);
+    }
+    /**
+     * 在访问controller前判断是否登录，返回json，不进行重定向。
+     *
+     * @param request
+     * @param response
+     * @return true-继续往下执行，false-该filter过滤器已经处理，不继续执行其他过滤器
+     * @throws Exception
+     */
+    @Override
+    protected boolean onAccessDenied(ServletRequest request, ServletResponse response) throws IOException {
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        //这里是个坑，如果不设置的接受的访问源，那么前端都会报跨域错误，因为这里还没到corsConfig里面
+        httpServletResponse.setHeader("Access-Control-Allow-Origin", ((HttpServletRequest) request).getHeader("Origin"));
+        httpServletResponse.setHeader("Access-Control-Allow-Credentials", "true");
+        httpServletResponse.setCharacterEncoding("UTF-8");
+        httpServletResponse.setContentType("application/json");
+        Map<String,Object> rs = new HashMap<>();
+        Map<String,Object> result  = new HashMap<>();
+        result.put("code",20403);
+        result.put("msg","登录信息失效，请重新登陆");
+        rs.put("result",result);
+        httpServletResponse.getWriter().write(JSONObject.toJSON(rs).toString());
+        return false;
+    }
+}
